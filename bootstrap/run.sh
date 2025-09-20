d7f0e334097c5bb721affa629e311b3631bc189d
Anton Curanz <anton@curanz.de>
2025-09-20 23:34:02 +0200

    rook

--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 cat initial-secrets.yaml | op inject | kubectl apply --server-side --field-manager flux-client-side-apply -f -
+
+flux bootstrap github \
+  --owner=antoncuranz \
+  --repository=cloudlab \
+  --branch=main \
+  --path=./cluster \
+  --personal
